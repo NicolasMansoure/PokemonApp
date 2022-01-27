cbcb52a08a9467de1ac1934902ce882043025ba8
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2022-01-27 17:04:02 +0100

    First commit + init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pokemon-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PokemonApp';
+
+export class AppComponent implements OnInit {
+  title = 'Liste de pokémons';
+  pokemons!: Pokemon[];
+  
+
+  ngOnInit(){
+    this.pokemons = POKEMONS;
+  }
+
+  
+  selectPokemon(pokemon: Pokemon) {
+    alert("Vous avez cliqué sur " + pokemon.name);
+  }
+
 }
